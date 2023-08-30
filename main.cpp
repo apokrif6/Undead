b328b35fa6208ef63e62ca750b643a6aa964f70b
apokrif6 <j.berenchan@gmail.com>
2023-08-30 23:36:55 +0200

    Init repo. Connect SFML and create CMake build file

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    int modeWidth = 640;
+    int modeHeight = 480;
+
+    sf::RenderWindow window(sf::VideoMode(modeWidth, modeHeight), "SFML Application");
+    sf::CircleShape shape;
+    shape.setRadius(40.f);
+    shape.setPosition((float)modeWidth / 2, (float)modeHeight /2);
+    shape.setFillColor(sf::Color::Magenta);
+
+    while (window.isOpen())
+    {
+        sf::Event event{};
+
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+}
